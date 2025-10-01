d7974774e0392d18c96ce104365472d8438c886e
Nikita <lisqxd@proton.me>
2025-10-02 00:53:59 +0200

    fix tests

--- test/Flip.test.tsx
+++ test/Flip.test.tsx
@@ -1,52 +1,30 @@
 /// <reference types="@vitest/browser/providers/playwright" />
 import { expect, test, vi } from "vitest"
 import { render } from "vitest-browser-react"
-import { Flip, FlipWrapper } from "../src/Flip.js"
+import { Flip } from "../src/Flip.js"
 
 test("Flip calls animateMove", async () => {
   const animateMove = vi.fn()
   const doc = render(
-    <Flip style={{ marginTop: 10 }} options={{ animateMove }}>
-      Test
-    </Flip>,
-  )
-  await expect.element(doc.getByText("Test")).toBeInTheDocument()
-  expect(animateMove).toHaveBeenCalledTimes(0)
-  doc.rerender(
-    <Flip style={{ marginTop: 20 }} options={{ animateMove }}>
-      Test
-    </Flip>,
-  )
-  expect(animateMove).toHaveBeenCalledTimes(1)
-  expect(animateMove.mock.calls[0]?.[0].style).toEqual({ translate: "0px -10px" })
-  doc.rerender(
-    <Flip style={{ marginTop: 20 }} options={{ animateMove }}>
-      Test
-    </Flip>,
-  )
-  expect(animateMove).toHaveBeenCalledTimes(1)
-})
-
-test("FlipWrapper calls animateMove", async () => {
-  const animateMove = vi.fn()
-  const doc = render(
-    <FlipWrapper animateMove={animateMove}>
+    <Flip animateMove={animateMove}>
       <div style={{ marginTop: 10 }}>Test</div>
-    </FlipWrapper>,
+    </Flip>,
   )
   await expect.element(doc.getByText("Test")).toBeInTheDocument()
   expect(animateMove).toHaveBeenCalledTimes(0)
   doc.rerender(
-    <FlipWrapper animateMove={animateMove}>
-      <div style={{ marginTop: 20 }}>Test</div>
-    </FlipWrapper>,
+    <Flip animateMove={animateMove}>
+      <div style={{ marginTop: 20 }}>Test2</div>
+    </Flip>,
   )
+  await expect.element(doc.getByText("Test2")).toBeInTheDocument()
   expect(animateMove).toHaveBeenCalledTimes(1)
   expect(animateMove.mock.calls[0]?.[0].style).toEqual({ translate: "0px -10px" })
   doc.rerender(
-    <FlipWrapper animateMove={animateMove}>
-      <div style={{ marginTop: 20 }}>Test</div>
-    </FlipWrapper>,
+    <Flip animateMove={animateMove}>
+      <div style={{ marginTop: 20 }}>Test3</div>
+    </Flip>,
   )
+  await expect.element(doc.getByText("Test3")).toBeInTheDocument()
   expect(animateMove).toHaveBeenCalledTimes(1)
 })
